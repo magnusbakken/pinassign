48b390987328157b39c90b2ccc61b4cb73282a69
Magnus Grindal Bakken <magnusbakken@gmail.com>
2015-09-12 15:55:21 +0200

    Separated game API from CLI.
    Separated the game API from the command line interface code. Also added
    some more commands.

--- pinassign_cli.py
+++ pinassign_cli.py
@@ -1,8 +1,6 @@
 import cmd
-import random
 
-import pinassign
-import simulation
+from game_api import *
 
 INTRO = """Welcome to the PinAssign command line interface.
 
@@ -14,8 +12,9 @@ To add a player, type "addplayer NAME".
 
 Player names must not contain spaces (use tags).
 
-Once you've added all machines and at least one player, type "start".
-After the game has been started you can no longer add machines.
+Once you've added all machines and at least one player, type "start". After the game has been started you can no longer add machines.
+
+Then use register PLAYERNAME MACHINENAME whenever a player has finished a machine.
 """
 
 GAME_STARTED = """The game has started!
@@ -31,137 +30,6 @@ GAME_RESET = """The game has been reset. New machines and players must be added.
 SCORES_RESET = """The game scores have been reset.
 
 The game is still running with the same machines and players as before, but no scores are registered anymore."""
-
-class GameError(Exception):
-    pass
-
-class MachineError(GameError):
-    pass
-
-class InvalidMachineError(MachineError):
-    pass
-
-class DuplicateMachineError(MachineError):
-    pass
-
-class UnknownMachineError(MachineError):
-    pass
-
-class PlayerError(GameError):
-    pass
-
-class InvalidPlayerError(PlayerError):
-    pass
-
-class DuplicatePlayerError(PlayerError):
-    pass
-
-class UnknownPlayerError(PlayerError):
-    pass
-
-class DuplicateScoreError(GameError):
-    pass
-
-class Game:
-    def __init__(self, r=random.Random()):
-        self.r = r
-        self._machines = []
-        self._players = []
-        self._scores = []
-        self._machine_dict = {}
-        self._player_dict = {}
-        self._is_running = False
-    
-    @property
-    def machines(self):
-        return self._machines
-    
-    @property
-    def players(self):
-        return self._machines
-    
-    @property
-    def scores(self):
-        return self._scores
-    
-    def add_machine(self, name, expected_time):
-        self._fail_if_running()
-        if not name:
-            raise InvalidMachineError('The machine must have a name')
-        elif expected_time <= 0:
-            raise InvalidMachineError('The expected time of the machine cannot be zero or negative')
-        elif self._get_machine(name) is not None:
-            raise DuplicateMachineError('The machine {} already exists'.format(name))
-        machine = pinassign.Machine(name, expected_time)
-        self._machines.append(machine)
-        self._machine_dict[name] = machine
-    
-    def add_player(self, name):
-        if not name:
-            raise InvalidPlayerError('The player must have a name')
-        elif ' ' in name:
-            raise InvalidPlayerError('Player names must not contain spaces')
-        elif self._get_player(name) is not None:
-            raise DuplicatePlayerError('The player {} already exists'.format(name))
-        player = pinassign.Player(name)
-        self._players.append(player)
-        self._player_dict[name] = player
-    
-    def start(self):
-        self._fail_if_running()
-        if not self._machines:
-            raise GameError('There must be at least one machine')
-        elif not self._players:
-            raise GameError('There must be at least one player')
-        self._is_running = True
-        return self._assign_all()
-    
-    def register_score(self, machine_name, player_name):
-        self._fail_if_not_running()
-        if not machine_name:
-            raise InvalidMachineError('No machine name given')
-        elif not player_name:
-            raise InvalidPlayerError('No player name given')
-        
-        machine = self._get_machine(machine_name)
-        if not machine:
-            raise UnknownMachineError('Machine {} not recognized'.format(machine_name))
-        player = self._get_player(player_name)
-        if not player:
-            raise UnknownPlayerError('Player {} not recognized'.format(player_name))
-        try:
-            return pinassign.player_finished_machine(
-                machine, player, self._machines, self._players, self._scores, self.r)
-        except ValueError as e:
-            msg = 'Score for {} on {} already registered'.format(player_name, machine_name)
-            raise DuplicateScoreError(msg) from e
-    
-    def is_finished(self):
-        self._fail_if_not_running()
-        return pinassign.is_everyone_finished(self._machines, self._players, self._scores)
-    
-    def reset_scores(self):
-        self._fail_if_not_running()
-        self._scores = []
-        self._is_running = False
-    
-    def _fail_if_running(self):
-        if self._is_running:
-            raise GameError('The game has already been started')
-    
-    def _fail_if_not_running(self):
-        if not self._is_running:
-            raise GameError('The game has not been started')
-    
-    def _assign_all(self):
-        assert self._is_running
-        return pinassign.assign_players(self._machines, self._players, self._scores, self.r)
-    
-    def _get_machine(self, name):
-        return self._machine_dict.get(name, None)
-    
-    def _get_player(self, name):
-        return self._player_dict.get(name, None)
 
 class PinAssignCmd(cmd.Cmd):
     def __init__(self):
@@ -309,6 +177,58 @@ After running this command, machines may be added. The start command must be use
             self.game.reset_scores()
         except GameError as e:
             print('Cannot reset scores: {}'.format(e))
+    
+    def do_playerready(self, s):
+        """Marks a player as ready.
+
+Use this command when players have played machines in a different way than recommended by the algorithm.
+
+For instance if, the algorithm recommended that Player1 play MachineA, but Player2 started playing it instead, execute playerready Player1 and playerbusy Player2.
+
+You can also use this command if the player was marked as temporarily unable to play with the playerbusy command, and is now back in business."""
+        try:
+            self.game.set_player_ready(True)
+        except GameError as e:
+            print('Cannot mark player as ready: {}'.format(e))
+    
+    def do_playerbusy(self, s):
+        """Marks a player as busy.
+
+Use this command when players have played machines in a different way than recommended by the algorithm.
+
+For instance if, the algorithm recommended that Player1 play MachineA, but Player2 started playing it instead, execute playerready Player1 and playerbusy Player2.
+
+You can also use this command if the player is temporarily unable to play."""
+        try:
+            self.game.set_player_ready(False)
+        except GameError as e:
+            print('Cannot mark player as busy: {}'.format(e))
+    
+    def do_machineready(self, s):
+        """Marks a machine as ready.
+
+Use this command when players have played machines in a different way than recommended by the algorithm.
+
+For instance if, the algorithm recommended that Player1 play MachineA, but he or she started MachineB instead, execute machineready MachineA and machinebusy MachineB.
+
+You can also use this command if the machine was marked as temporarily out of order with the machinebusy command, and is now back in business."""
+        try:
+            self.game.set_machine_ready(True)
+        except GameError as e:
+            print('Cannot mark machine as ready: {}'.format(e))
+    
+    def do_machinebusy(self, s):
+        """Marks a machine as busy.
+
+Use this command when players have played machines in a different way than recommended by the algorithm.
+
+For instance if, the algorithm recommended that Player1 play MachineA, but he or she started MachineB instead, execute machineready MachineA and machinebusy MachineB.
+
+You can also use this command if the machine is temporarily out of order."""
+        try:
+            self.game.set_machine_ready(False)
+        except GameError as e:
+            print('Cannot mark machine as busy: {}'.format(e))
     
     def _print_assignments(self, assignments):
         for idx, (machine, player) in enumerate(assignments):
